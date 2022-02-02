98cfeb6f5faa25b0657b8e43dff13dd400656182
R-star-1 <97829073+R-star-1@users.noreply.github.com>
2022-02-02 18:22:43 +0530

    Add files via upload

---
+++ Task1 (1).py
@@ -0,0 +1,15 @@
+
+#Ritik Kumar
+# Day 1 Task
+
+# print("Enter a size of the list")
+
+
+N= int(input())
+Sum=0;
+# print("Enter a data in a list")
+for i in range(0,N):
+    data= int(input())
+    Sum = Sum + data
+    
+print(Sum)
